87b6a6b3d370d1ac00c151f7f4f5956a9bb5085d
Mateusz Kiebala <mateuszkiebala@gmail.com>
2018-10-14 15:24:19 +0200

    Init

---
+++ library/src/main/scala/MinimalAlgorithm.scala
@@ -0,0 +1,7 @@
+class MinimalAlgorithm {
+  var objects = List[MinimalAlgorithmObject]()
+
+  def importObjects(objects: List[MinimalAlgorithmObject]): Unit = {
+    this.objects = objects
+  }
+}
---
+++ library/src/main/scala/MinimalAlgorithmObject.scala
@@ -0,0 +1,4 @@
+class MinimalAlgorithmObject(o: Object, weight: Int) extends Ordered[MinimalAlgorithmObject] {
+  def compare(mao: MinimalAlgorithmObject): Int = this.weight
+
+}
