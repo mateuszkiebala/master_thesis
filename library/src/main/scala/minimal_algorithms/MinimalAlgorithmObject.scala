9d4e28d4ede673ae8688037da3a169ef50e48f3d
Mateusz Kiebala <mateuszkiebala@gmail.com>
2019-02-19 17:30:53 +0100

    Small comment

--- library/src/main/scala/minimal_algorithms/MinimalAlgorithmObject.scala
+++ library/src/main/scala/minimal_algorithms/MinimalAlgorithmObject.scala
@@ -1,6 +1,19 @@
 package minimal_algorithms
 
+/**
+  * Interface for the most basic object which can be used in Minimal Algorithm.
+  * @tparam Self
+  */
 trait MinimalAlgorithmObject[Self <: MinimalAlgorithmObject[Self]] extends Comparable[Self] with Serializable { self: Self =>
+  /**
+    * Provides integer value that represents weight of the object.
+    * @return Weight of the object.
+    */
   def getWeight: Int
+
+  /**
+    * Provides integer value that represents weight of the object.
+    * @return
+    */
   def sortValue: Int
 }
