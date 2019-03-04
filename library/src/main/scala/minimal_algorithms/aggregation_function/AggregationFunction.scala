33a16776cf7f87373d6920b4b381d8d30a5244e8
Mateusz Kiebala <mateuszkiebala@gmail.com>
2019-03-04 23:06:53 +0100

    Update docs

--- library/src/main/scala/minimal_algorithms/aggregation_function/AggregationFunction.scala
+++ library/src/main/scala/minimal_algorithms/aggregation_function/AggregationFunction.scala
@@ -1,7 +1,19 @@
 package minimal_algorithms.aggregation_function
 
 abstract class AggregationFunction extends Serializable {
+  /**
+    * @return Default value for given aggregation function. It is a start value for iteration ex. foldLeft(defaultValue){...}
+    *         and as well it is used in RangeTree initialization.
+    */
   def defaultValue: Int
+
+  /**
+    * @return Aggregation function ex. (x: Int, y: Int) => x + y
+    */
   def apply: (Int, Int) => Int
+
+  /**
+    * @return Flag indicating whether we should use average result or not.
+    */
   def average: Boolean
 }
