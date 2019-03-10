5de942305bbc4b0f43d4102287e8bb30510f1c8e
Mateusz Kiebala <mateuszkiebala@gmail.com>
2019-03-10 16:37:34 +0100

    tmp

---
+++ library/src/test/distributed/group_by/generator.py
@@ -0,0 +1,52 @@
+from random import randint
+from itertools import groupby
+import os
+
+if not os.path.exists('input'):
+    os.makedirs('input')
+
+SUM_OUT = 'output_sum'
+MIN_OUT = 'output_min'
+MAX_OUT = 'output_max'
+AVG_OUT = 'output_avg'
+NUM_OF_PARTITIONS = 10
+NUM_OF_ELEMENTS = 50
+NUM_OF_ELEMENTS_PER_PARTITION = int(NUM_OF_ELEMENTS / NUM_OF_PARTITIONS)
+
+for d in [SUM_OUT, MIN_OUT, MAX_OUT, AVG_OUT]:
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+input_data = [(randint(1, 5), randint(-100, 5000)) for n in range(NUM_OF_ELEMENTS)]
+#for n in range(NUM_OF_PARTITIONS):
+#    with open('input/input_{0}.txt'.format(n), 'w') as file:
+#        for m in range(NUM_OF_ELEMENTS / NUM_OF_PARTITIONS):
+#            o = input_data[m * (n + 1)]
+#            file.write("{0}, {1}\n".format(o[0], o[1]))
+
+i = 0
+result_sum = {}
+result_min = {}
+result_max = {}
+result_avg = {}
+for key, group in groupby(sorted(input_data), lambda x: x[0]):
+    g = [x[1] for x in group]
+    result_sum[key] = sum(g)
+    result_min[key] = min(g)
+    result_max[key] = max(g)
+    result_avg[key] = sum(g) / float(len(g))
+
+
+def write_output(dir_name, data):
+    keys = list(sorted(data.keys()))
+    for n in range(NUM_OF_PARTITIONS):
+        with open('{0}/output_{1}.txt'.format(dir_name, n), 'w') as file:
+            for m in range(NUM_OF_ELEMENTS_PER_PARTITION):
+                index = n * NUM_OF_ELEMENTS_PER_PARTITION + m
+                print(index, len(keys))
+                if index > len(keys):
+                    return
+                key = keys[index]
+                file.write("{0}, {1}\n".format(key, data[key]))
+
+write_output(SUM_OUT, result_sum)
