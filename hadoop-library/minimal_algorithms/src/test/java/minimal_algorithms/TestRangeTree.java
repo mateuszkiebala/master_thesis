1be737f95c2353ee91e3126a76642ebc961b7978
Mateusz Kiebala <mateuszkiebala@gmail.com>
2019-05-14 21:19:59 +0200

    Add rangeTree unit tests

--- hadoop-library/minimal_algorithms/src/test/java/minimal_algorithms/TestRangeTree.java
+++ hadoop-library/minimal_algorithms/src/test/java/minimal_algorithms/TestRangeTree.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.ArrayList;
 import javafx.util.Pair;
 import org.junit.Assert;
+import org.junit.rules.ExpectedException;
+import org.apache.avro.AvroRuntimeException;
 import minimal_algorithms.examples.types.SumStatisticsAggregator;
 import minimal_algorithms.statistics.StatisticsAggregator;
 import minimal_algorithms.RangeTree;
 
 public class TestRangeTree {
+
+    @org.junit.Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
     @org.junit.Test
     public void testOneNodeTree() {
@@ -20,8 +25,26 @@ public class TestRangeTree {
         Assert.assertArrayEquals(expected, unpackSumResult(tree.getNodes()));
     }
 
-    @org.junit.Test(expected = org.apache.avro.AvroRuntimeException.class)
-    public void testPositionOutOfRange() {
+    @org.junit.Test
+    public void testStartGtEnd() throws AvroRuntimeException {
+        expectedEx.expect(AvroRuntimeException.class);
+        expectedEx.expectMessage("Start (3) greater than end (2)");
+
+        List<Pair<StatisticsAggregator, Integer>> elements = new ArrayList<>();
+        elements.add(new Pair(new SumStatisticsAggregator(10), 0));
+        elements.add(new Pair(new SumStatisticsAggregator(30), 1));
+        elements.add(new Pair(new SumStatisticsAggregator(20), 2));
+        elements.add(new Pair(new SumStatisticsAggregator(60), 3));
+        RangeTree tree = new RangeTree(elements);
+
+        tree.query(3, 2);
+    }
+
+    @org.junit.Test
+    public void testPositionOutOfRange() throws AvroRuntimeException {
+        expectedEx.expect(AvroRuntimeException.class);
+        expectedEx.expectMessage("Position out of range: 4");
+
         List<Pair<StatisticsAggregator, Integer>> elements = new ArrayList<>();
         elements.add(new Pair(new SumStatisticsAggregator(10), 0));
         elements.add(new Pair(new SumStatisticsAggregator(30), 4));
@@ -30,12 +53,63 @@ public class TestRangeTree {
         RangeTree tree = new RangeTree(elements);
     }
 
+    @org.junit.Test
+    public void testSum() {
+        List<Pair<StatisticsAggregator, Integer>> elements = new ArrayList<>();
+        elements.add(new Pair(new SumStatisticsAggregator(3), 2));
+        elements.add(new Pair(new SumStatisticsAggregator(2), 1));
+        elements.add(new Pair(new SumStatisticsAggregator(1), 0));
+        elements.add(new Pair(new SumStatisticsAggregator(4), 3));
+
+        RangeTree tree = new RangeTree(elements);
+        Integer[] expected = {null, 10, 3, 7, 1, 2, 3, 4};
+        Assert.assertArrayEquals(expected, unpackSumResult(tree.getNodes()));
+    }
+
+    @org.junit.Test
+    public void testSumBaseNotPowerOfTwo() {
+        List<Pair<StatisticsAggregator, Integer>> elements = new ArrayList<>();
+        elements.add(new Pair(new SumStatisticsAggregator(4), 2));
+        elements.add(new Pair(new SumStatisticsAggregator(1), 1));
+        elements.add(new Pair(new SumStatisticsAggregator(3), 0));
+        elements.add(new Pair(new SumStatisticsAggregator(10), 3));
+        elements.add(new Pair(new SumStatisticsAggregator(2), 4));
+
+        RangeTree tree = new RangeTree(elements);
+        Integer[] expected = {null, 20, 18, 2, 4, 14, 2, null, 3, 1, 4, 10, 2, null, null, null};
+        Assert.assertEquals(8, tree.getBASE());
+        Assert.assertArrayEquals(expected, unpackSumResult(tree.getNodes()));
+    }
+
+    @org.junit.Test
+    public void testQuerySum() {
+        List<Pair<StatisticsAggregator, Integer>> elements = new ArrayList<>();
+        elements.add(new Pair(new SumStatisticsAggregator(4), 2));
+        elements.add(new Pair(new SumStatisticsAggregator(1), 1));
+        elements.add(new Pair(new SumStatisticsAggregator(3), 0));
+        elements.add(new Pair(new SumStatisticsAggregator(10), 3));
+        elements.add(new Pair(new SumStatisticsAggregator(2), 4));
+
+        RangeTree tree = new RangeTree(elements);
+        Assert.assertEquals(new Integer(3), unpackSumSA(tree.query(0, 0)));
+        Assert.assertEquals(new Integer(8), unpackSumSA(tree.query(0, 2)));
+        Assert.assertEquals(new Integer(18), unpackSumSA(tree.query(0, 3)));
+        Assert.assertEquals(new Integer(20), unpackSumSA(tree.query(0, 4)));
+        Assert.assertEquals(new Integer(14), unpackSumSA(tree.query(2, 3)));
+        Assert.assertEquals(new Integer(17), unpackSumSA(tree.query(1, 4)));
+        Assert.assertEquals(new Integer(16), unpackSumSA(tree.query(2, 5)));
+    }
+
     private Integer[] unpackSumResult(StatisticsAggregator[] nodes) {
         Integer[] result = new Integer[nodes.length];
         for (int i = 0; i < nodes.length; i++) {
-            StatisticsAggregator s = nodes[i];
-            result[i] = s == null ? null : (int) s.get(0);
+            StatisticsAggregator sa = nodes[i];
+            result[i] = unpackSumSA(sa);
         }
         return result;
+    }
+
+    private Integer unpackSumSA(StatisticsAggregator sa) {
+        return sa == null ? null : (int) sa.get(0);
     }
 }
