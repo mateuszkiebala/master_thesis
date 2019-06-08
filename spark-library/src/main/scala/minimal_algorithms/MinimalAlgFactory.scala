593fb845c5c2fae4adf9635815b1deb3ac9a7ea7
Mateusz Kiebala <mateuszkiebala@gmail.com>
2019-06-08 22:31:23 +0200

    MinimalAlgFactory spark without SemiJoin

---
+++ spark-library/src/main/scala/minimal_algorithms/MinimalAlgFactory.scala
@@ -0,0 +1,42 @@
+package minimal_algorithms
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import scala.reflect.ClassTag
+import minimal_algorithms.statistics.StatisticsAggregator
+import minimal_algorithms.group_by.MinimalGroupBy
+import minimal_algorithms.sliding_aggregation.MinimalSlidingAggregation
+
+class MinimalAlgFactory[T](spark: SparkSession, numPartitions: Int, input: RDD[T], itemsCnt: Int = -1)(implicit ttag: ClassTag[T]) {
+
+  def teraSort[K](cmpKey: T => K)(implicit ord: Ordering[K], ktag: ClassTag[K]): RDD[T] = {
+    new MinimalAlgorithm(spark, numPartitions).teraSort(input, cmpKey);
+  }
+
+  def rank[K](cmpKey: T => K)(implicit ord: Ordering[K], ktag: ClassTag[K]): RDD[(Int, T)] = {
+    new MinimalAlgorithm(spark, numPartitions).rank(input, cmpKey);
+  }
+
+  def perfectSort[K](cmpKey: T => K)(implicit ord: Ordering[K], ktag: ClassTag[K]): RDD[T] = {
+    new MinimalAlgorithm(spark, numPartitions).perfectSort(input, cmpKey, itemsCnt);
+  }
+
+  def perfectSortWithRanks[K](cmpKey: T => K)(implicit ord: Ordering[K], ktag: ClassTag[K]): RDD[(Int, T)] = {
+    new MinimalAlgorithm(spark, numPartitions).perfectSortWithRanks(input, cmpKey, itemsCnt);
+  }
+
+  def prefix[K, S <: StatisticsAggregator[S]](cmpKey: T => K, statsAgg: T => S)
+      (implicit ord: Ordering[K], ktag: ClassTag[K], stag: ClassTag[S]): RDD[(S, T)] = {
+    new MinimalAlgorithm(spark, numPartitions).prefix(input, cmpKey, statsAgg);
+  }
+
+  def groupBy[K, S <: StatisticsAggregator[S]](cmpKey: T => K, statsAgg: T => S)
+      (implicit ord: Ordering[K], ktag: ClassTag[K], stag: ClassTag[S]): RDD[(K, S)] = {
+    new MinimalGroupBy(spark, numPartitions).groupBy(input, cmpKey, statsAgg, itemsCnt);
+  }
+
+  def slidingAggregation[K, S <: StatisticsAggregator[S]](windowLength: Int, cmpKey: T => K, statsAgg: T => S)
+      (implicit ord: Ordering[K], ktag: ClassTag[K], stag: ClassTag[S]): RDD[(T, S)] = {
+    new MinimalSlidingAggregation(spark, numPartitions).aggregate(input, windowLength, cmpKey, statsAgg, itemsCnt)
+  }
+}
