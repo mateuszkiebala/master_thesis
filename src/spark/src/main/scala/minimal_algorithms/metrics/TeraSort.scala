97d4a051e1b43647260843b1804f1bb20ecdaf4b
Mateusz Kiebala <mateuszkiebala@gmail.com>
2019-07-22 23:50:10 +0200

    Spark TeraSort

---
+++ src/spark/src/main/scala/minimal_algorithms/metrics/TeraSort.scala
@@ -0,0 +1,15 @@
+package minimal_algorithms.spark.metrics
+
+import minimal_algorithms.spark.MinimalAlgorithm
+import org.apache.spark.sql._
+
+object TeraSortTest {
+
+  def main(args: Array[String]): Unit = Utils.time {
+    val spark = SparkSession.builder().appName("TeraSortTest").master("local").getOrCreate()
+    val df = spark.read.format("com.databricks.spark.avro").option("header", "true").load("/user/mati/data.avro")
+    val cmpKey = (o: Row) => o.getAs[Long]("long_prim")
+    val result = new MinimalAlgorithm(spark, 2).rank(df.rdd, cmpKey)
+    spark.stop()
+  }
+}
